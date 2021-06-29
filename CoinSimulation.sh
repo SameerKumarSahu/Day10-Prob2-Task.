752f771f37e069bd8653059530b353f01f883f0c
SameerKumarSahu <sameerkumarsahu0@gmail.com>
2021-06-29 23:30:41 +0530

    Modified the earlier UC2

--- CoinSimulation.sh
+++ CoinSimulation.sh
@@ -40,3 +40,39 @@ else
         echo "Tails Win by " $b
 
 fi
+
+
+
+
+Heads=0
+Tails=0
+
+while [[ $Heads -lt 21  &&
+		 $Tails -lt 21 ]]
+do
+	x=$((RANDOM%2))
+        if [ $x -eq 0 ]
+
+        then
+               ((Heads++))
+
+        else
+
+               ((Tails++))
+        fi
+done
+
+echo "Number of time Heads are" $Heads
+echo "Number of time Tails are" $Tails
+
+if [[ $Heads -eq 21 ]]
+then
+	s=$(($Heads - $Tails))        
+	echo "Heads Win by" $s
+elif [[ $Tails -eq 21 ]]
+then
+	s=$(($Tails - $Heads))
+        echo "Tails Win by " $s
+else
+        echo "Tie"
+fi
