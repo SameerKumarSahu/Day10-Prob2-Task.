47a57e7ba34c60b937b5e4f794fd4a8b543242d2
SameerKumarSahu <sameerkumarsahu0@gmail.com>
2021-06-29 23:14:50 +0530

    Flipping the coin

--- CoinSimulation.sh
+++ CoinSimulation.sh
@@ -1,2 +1,11 @@
 #!/bin/bash -x
 echo "Welcome to Filp Coin Simulation...!"
+
+
+x=$((RANDOM%2))
+    if [ $x -eq 0 ]
+    then
+         echo " Head come "
+    else
+          echo " Tail come "
+    fi
